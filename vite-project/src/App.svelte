d4b046a3c1cff3cc8ecd32ffa3d4ed598411e800
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-07-29 08:56:07 +0000

    yml OK!

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -19,7 +19,7 @@
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Попробуйте <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
 
   <p class="read-the-docs">
